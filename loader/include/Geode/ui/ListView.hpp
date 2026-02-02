5437a8f373e82fd1d9b0578e455149eadff6532b
aloaf <toastedhuman12345@gmail.com>
2026-02-01 19:36:00 -0600

    CCTableViewCell my beloved

--- loader/include/Geode/ui/ListView.hpp
+++ loader/include/Geode/ui/ListView.hpp
@@ -2,10 +2,10 @@
 
 #include <Geode/binding/CustomListView.hpp>
 #include <Geode/binding/CCIndexPath.hpp>
-#include <Geode/binding/TableViewCell.hpp>
+#include <Geode/binding/CCTableViewCell.hpp>
 
 namespace geode {
-    class GEODE_DLL GenericListCell : public TableViewCell {
+    class GEODE_DLL GenericListCell : public CCTableViewCell {
     protected:
         cocos2d::ccColor3B m_primaryColor;
         cocos2d::ccColor3B m_secondaryColor;
@@ -38,8 +38,8 @@ namespace geode {
         cocos2d::ccColor4B m_cellBorderColor;
 
         void setupList() override;
-        TableViewCell* getListCell(char const* key) override;
-        void loadCell(TableViewCell* cell, int index) override;
+        CCTableViewCell* getListCell(char const* key) override;
+        void loadCell(CCTableViewCell* cell, int index) override;
         void updateAllCells();
 
     public:
